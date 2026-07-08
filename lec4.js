e0eb85e897617d247e5c1274b10e4d50a2827c19
PriyanshuHaldar <priyanshuhaldar2004@gmail.com>
2026-07-08 23:04:34 +0530

    with operations

--- lec4.js
+++ lec4.js
@@ -21,3 +21,41 @@ let stringNumber=String(someNumber)
 console.log(stringNumber)
 
 console.log( typeof stringNumber)
+
+
+/*           OPERATIONS                        */
+
+
+let value=3
+let negValue=-value
+console.log(negValue)
+
+let str1="Hello"
+let str2= " ella"
+let str3=str1+str2
+console.log(str3)
+
+/*  Arithmetic operations
+
+console.log(3+4)
+console.log(2-1)
+console.log(3*4)
+console.log(4**2)
+console.log(4/2)
+console.log(4%2)
+
+*/
+
+/*
+
+console.log(3 + 4)
+console.log("3" + 4)
+console.log(3 + "4")
+console.log("3" + 4 + 2)
+console.log(3 + 4 + "2")
+
+*/
+
+let gameCounter=100
+let newgameCounter=gameCounter++;
+console.log(newgameCounter,gameCounter)
