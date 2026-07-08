bec971ce78fd122c105acc07d3f402f26b9f4150
PriyanshuHaldar <priyanshuhaldar2004@gmail.com>
2026-07-08 22:21:48 +0530

    conversion of datatype

---
+++ lec4.js
@@ -0,0 +1,23 @@
+
+// conversion of string to number
+
+let scope="33abc"
+console.log( typeof scope)
+
+let valueInNumber=Number(scope)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+// Boolean conversion
+
+let isloggedIn="ella"
+
+let booleanIloggedIn=Boolean(isloggedIn)
+console.log( typeof booleanIloggedIn)
+
+let someNumber=33
+
+let stringNumber=String(someNumber)
+console.log(stringNumber)
+
+console.log( typeof stringNumber)
